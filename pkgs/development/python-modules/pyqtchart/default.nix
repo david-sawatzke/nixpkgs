c907c492df794ef5e55a68a8b6ef4b6885a4d433
David Sawatzke <d-git@sawatzke.dev>
2020-06-05 01:10:19 +0200

    [WIP] make pyqtchart compile

--- pkgs/development/python-modules/pyqtchart/default.nix
+++ pkgs/development/python-modules/pyqtchart/default.nix
@@ -1,24 +1,64 @@
-{ stdenv, lib, fetchurl, pkgs, qt5, python3Packages }:
+{ lib, python3Packages, pkgconfig, qmake, qtbase, qtsvg, qtcharts
+, wrapQtAppsHook }:
 
-stdenv.mkDerivation rec {
-  pname = "pyqtchart";
-  version = "5.12";
+let
 
-  src = fetchurl {
-    url = "https://www.riverbankcomputing.com/static/Downloads/PyQtChart/${version}/PyQtChart_gpl-5.12.tar.gz";
-    sha256 = "1my07cksnsxbcc7h7i0429xj712jrs3sqwdmifjv3pw1kyhcdsmc";
+  inherit (python3Packages) buildPythonPackage python pyqt5;
+  inherit (pyqt5) sip;
+
+in buildPythonPackage rec {
+  pname = "PyQtChart";
+  version = "5.15.0";
+  format = "other";
+
+  src = python3Packages.fetchPypi {
+    inherit pname version;
+    sha256 = "0qb6fmfg9n9wpvraqrdzzqpwgqdqi1msrfqrs1cqiisrcyb1lsvr";
   };
 
-  nativeBuildInputs = [
-    qt5.qmake
-    python3Packages.pyqt5
-  ];
+  outputs = [ "out" "dev" ];
 
-  buildInputs = with pkgs; [
-    python
-  ];
+  nativeBuildInputs = [ pkgconfig qmake sip qtbase qtsvg qtcharts ];
+
+  buildInputs = [ sip qtbase qtsvg qtcharts ];
+
+  propagatedBuildInputs = [ pyqt5 ];
+
+  postPatch = ''
+    substituteInPlace configure.py \
+      --replace \
+      "target_config.py_module_dir" \
+      "'$out/${python.sitePackages}'"
+  '';
 
   configurePhase = ''
-    python ./configure.py
+    runHook preConfigure
+    mkdir -p "$out/share/sip/PyQt5"
+    ${python.executable} configure.py -w \
+      --destdir="$out/${python.sitePackages}/PyQt5" \
+      --qtchart-sipdir=$out/share/sip/PyQt5 \
+      --pyqt-sipdir="${pyqt5}/share/sip/PyQt5" \
+      --apidir="$out/api/${python.libPrefix}" \
+      --stubsdir="$out/${python.sitePackages}/PyQt5"
+    runHook postConfigure
   '';
+
+  postInstall = ''
+    # Let's make it a namespace package
+    cat << EOF > $out/${python.sitePackages}/PyQt5/__init__.py
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
+    EOF
+  '';
+
+  enableParallelBuilding = true;
+
+  passthru = { inherit wrapQtAppsHook; };
+
+  meta = with lib; {
+    description = "Python bindings for Qt5";
+    homepage = "http://www.riverbankcomputing.co.uk";
+    license = licenses.gpl3;
+    platforms = platforms.mesaPlatforms;
+  };
 }
